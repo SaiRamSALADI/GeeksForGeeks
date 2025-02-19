2625610c6e13807d0c26a1769975c770d112b4c1
S V VENKATA SAI RAM <22mh1a42i7@acoe.edu.in>
2025-02-19 10:35:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Prime Number/prime-number.java
+++ Difficulty: Basic/Prime Number/prime-number.java
@@ -30,7 +30,7 @@ class GFG {
 class Solution {
     static boolean isPrime(int n) {
         // code here
-        for(int i=2;i<n;i++){
+        for(int i = 2; i<n; i++) {
             if(n%i==0) {
                 return false;
             }
